f612198e0dbb616ed99a6b98b5d402be627c327c
Selam Gessese <sygessese@gmail.com>
2019-09-24 18:23:12 -0700

    search bar and header visible

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Search from './Search.js';
+import Header from './Header.js';
 
 const App = () => {
   return (
-    <div>Movie list!
-    <Search />
+    <div>
+      <Header />
+      <Search />
     </div>
   )
 }
--- src/Search.js
+++ src/Search.js
@@ -5,7 +5,7 @@ const Search = () => {
     <div>
     <form className="form-inline my-2 my-lg-0">
     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <a className="btn btn-primary btn-lg" href="#" role="button">Go!</a>
     </form>
     </div>
   )
