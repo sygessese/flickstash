05ca7b164a5156fa11115f905f910d10ffe0aeb2
Selam Gessese <sygessese@gmail.com>
2019-09-24 19:37:18 -0700

    search is alerting properly

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Search from './Search.js';
 import Header from './Header.js';
+import Movies from './Movies.js';
+
 
 const App = () => {
   return (
-    <div>
+    <div className="jumbotron">
       <Header />
       <Search />
+      <Movies />
     </div>
   )
 }
--- src/Search.js
+++ src/Search.js
@@ -4,11 +4,14 @@ const Search = () => {
   return (
     <div>
     <form className="form-inline my-2 my-lg-0">
-    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-    <a className="btn btn-primary btn-lg" href="#" role="button">Go!</a>
+    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" id=
+    "search"></input>
+    <a className="btn btn-primary btn-lg" href="#" role="button" onClick={()=>{alert(document.getElementById("search").value)}}>Go!</a>
     </form>
     </div>
   )
 }
 
 export default Search;
+
+//() => this.setState({showDescription: !this.state.showDescription})
